2155c7164b28302cbc7be98209e68e9dc1709bcb
PabloDQuiroga <jtdoe78@gmail.com>
2021-11-07 09:20:07 -0300

    Added first Firebase Analitycs Event

--- app/src/main/java/com/siar/demofull/MainActivity.kt
+++ app/src/main/java/com/siar/demofull/MainActivity.kt
@@ -2,10 +2,17 @@ package com.siar.demofull
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Analitycs Events
+        val analitycs = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("Message", "Integracion de Firebase completa")
+        analitycs.logEvent("InitScreen", bundle)
     }
 }
