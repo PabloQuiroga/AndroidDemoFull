fadd56981dd04cb6c81e6cfefe3dee5a8983fb17
Pablo Daniel Quiroga <pquiroga@fusap.com.ar>
2021-12-20 16:40:40 -0300

    Added manager for remote storage from Firestore Database

--- app/src/main/java/com/siar/demofull/HomeActivity.kt
+++ app/src/main/java/com/siar/demofull/HomeActivity.kt
@@ -3,10 +3,11 @@ package com.siar.demofull
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.crashlytics.FirebaseCrashlytics
+import com.google.firebase.firestore.FirebaseFirestore
 import com.siar.demofull.databinding.ActivityHomeBinding
-
 
 enum class ProviderType {
     BASIC,
@@ -15,6 +16,7 @@ enum class ProviderType {
 
 class HomeActivity : AppCompatActivity() {
 
+    private val db = FirebaseFirestore.getInstance()
     private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -60,5 +62,51 @@ class HomeActivity : AppCompatActivity() {
 
             throw RuntimeException("Test Crash by Pablo") // Forzado de crash
         }
+
+        binding.setButton.setOnClickListener {
+            db.collection("users").document(email).set(
+                hashMapOf(
+                    "provider" to provider,
+                    "address" to binding.editTextAddress.text.toString(),
+                    "phone" to binding.editTextPhone.text.toString()
+                )
+            )
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        showDialog("Exito", "Los datos se han guardado correctamente")
+                    } else {
+                        showDialog("Error", "Se ha producido un error al guardar los datos")
+                    }
+                }
+        }
+        binding.getButton.setOnClickListener {
+            db.collection("users").document(email).get().addOnSuccessListener {
+                if (it.exists()){
+                    binding.editTextAddress.setText(it.get("address") as String?)
+                    binding.editTextPhone.setText(it.get("phone") as String?)
+                }else{
+                    showDialog("Error", "Se ha producido un error al cargar los datos")
+                }
+
+            }
+        }
+        binding.deleteButton.setOnClickListener {
+            db.collection("users").document(email).delete().addOnCompleteListener {
+                if (it.isSuccessful) {
+                    showDialog("Exito", "Los datos se han borrado correctamente")
+                } else {
+                    showDialog("Error", "Se ha producido un error al borrar los datos")
+                }
+            }
+        }
+    }
+
+    private fun showDialog(title: String, message: String) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+        builder.setMessage(message)
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
     }
 }
