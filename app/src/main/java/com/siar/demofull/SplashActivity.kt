5f7f5b56f0c3ff9c18c4e4cb81c93625801cf809
PabloDQuiroga <jtdoe78@gmail.com>
2022-07-19 10:59:05 -0300

    refactor for clean

--- app/src/main/java/com/siar/demofull/SplashActivity.kt
+++ app/src/main/java/com/siar/demofull/SplashActivity.kt
@@ -4,11 +4,14 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.siar.demofull.ui.MainActivity
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         startActivity(Intent(this, MainActivity::class.java))
     }
 }
--- app/src/main/java/com/siar/demofull/ui/MainActivity.kt
+++ app/src/main/java/com/siar/demofull/ui/MainActivity.kt
@@ -1,11 +1,20 @@
-package com.siar.demofull
+package com.siar.demofull.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.siar.demofull.BuildConfig
+import com.siar.demofull.R
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        const val TAG = "MainActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.e(TAG, BuildConfig.BASE_URL)
     }
 }
