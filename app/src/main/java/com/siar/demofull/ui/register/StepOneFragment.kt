2bcf0a5dc158336a517a1102567247d8adad8e84
PabloDQuiroga <jtdoe78@gmail.com>
2022-08-13 09:22:16 -0300

    Added icons and rebase register fragment -to test it-

--- app/src/main/java/com/siar/demofull/ui/register/StepOneFragment.kt
+++ app/src/main/java/com/siar/demofull/ui/register/StepOneFragment.kt
@@ -5,14 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.siar.demofull.R
+import com.siar.demofull.databinding.FragmentStepOneBinding
 
 class StepOneFragment : Fragment() {
+    private lateinit var binding: FragmentStepOneBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_step_one, container, false)
+        binding = FragmentStepOneBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        showAppBar()
+    }
+
+    private fun showAppBar() {
+        binding.registerToolbar.title = "Nuevo Registro"
     }
 }
