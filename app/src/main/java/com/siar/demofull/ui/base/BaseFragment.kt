42cb948bec37ab3ec7307ab5fcd5586c6ee704da
Pablo Quiroga <jtdoe78@gmail.com>
2022-08-15 09:52:55 -0300

    Generic method for hide and show Appbar and handled navigation

--- app/src/main/java/com/siar/demofull/ui/base/BaseFragment.kt
+++ app/src/main/java/com/siar/demofull/ui/base/BaseFragment.kt
@@ -1,4 +1,22 @@
 package com.siar.demofull.ui.base
 
-class BaseFragment {
+import android.view.View
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.siar.demofull.R
+
+open class BaseFragment: Fragment() {
+
+    /* backButton en false para deshabilitar icono de regresar */
+    open fun showAppbar(v: View, title: String, backButton: Boolean){
+        with(v as Toolbar){
+            this.title = title
+            if(backButton) this.navigationIcon = ContextCompat.getDrawable(requireContext(), R.drawable.ic_back_arrow)
+        }
+    }
+
+    open fun hideAppbar(v: View, hidden: Boolean){
+        (v as Toolbar).visibility = View.GONE
+    }
 }
--- app/src/main/java/com/siar/demofull/ui/register/StepOneFragment.kt
+++ app/src/main/java/com/siar/demofull/ui/register/StepOneFragment.kt
@@ -1,19 +1,34 @@
 package com.siar.demofull.ui.register
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import com.siar.demofull.R
 import com.siar.demofull.databinding.FragmentStepOneBinding
+import com.siar.demofull.ui.base.BaseFragment
 
-class StepOneFragment : Fragment() {
+class StepOneFragment : BaseFragment() {
     private lateinit var binding: FragmentStepOneBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        /* maneja el back del sistema */
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                Log.e("BACKBUTTON", "Back button clicks")
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentStepOneBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -21,10 +36,11 @@ class StepOneFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        showAppBar()
-    }
-
-    private fun showAppBar() {
-        binding.registerToolbar.title = "Nuevo Registro"
+        with(binding.registerToolbar){
+            showAppbar(this,getString(R.string.register_toolbar), true)
+            this.setNavigationOnClickListener {
+                Log.e("Navegacion toolbar...", "va atras")
+            }
+        }
     }
 }
